fd78ae44dd48be61460c0b5857d4ec163edc5a61
Ramiro <ramifazio@gmail.com>
2022-11-25 10:29:06 -0300

    First Commit server separated

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const server = require("./src/app.js");
+const { db } = require("./src/db.js");
+
+//* Pisa la DB cada vez que se salva el Backend
+db.sync({ force: true }).then(() => {
+  server.listen(3030, () => {
+    console.log("--->   Servidor en puerto 3030"); // eslint-disable-line no-console
+  });
+});
