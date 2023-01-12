ebc715e83f9bf7a96d19b2717f1c8e7cb8a403de
Ramiro <ramifazio@gmail.com>
2023-01-12 11:17:25 -0300

    Remove filter with DNI of integrantes

--- src/controllers/addIntegrantes.js
+++ src/controllers/addIntegrantes.js
@@ -5,17 +5,9 @@ addIntegrantes.post("/", async (req, res) => {
   try {
     const integrantes = req.body;
 
-    const response = [];
+    const newIntegrante = await Integrante.bulkCreate(integrantes);
 
-    integrantes.forEach(async (integrante) => {
-      const res = await Integrante.findOne({ where: { dni: integrante.dni } });
-      if (!res) {
-        const newIntegrante = await Integrante.create(integrante);
-        response.push(newIntegrante);
-      }
-    });
-
-    return res.status(200).json(response);
+    return res.status(200).json(newIntegrante);
   } catch (err) {
     console.log(err);
   }
