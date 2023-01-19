4eedc1cc008b6d96a19cfb5a5378df082658e518
Ramiro <ramifazio@gmail.com>
2023-01-19 13:38:33 -0300

    Update credentials

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
   PORT: 3001,
   DB_HOST: "localhost",
-  DB_USER: "root",
-  DB_PASSWORD: "12345",
+  DB_USER: "root" || "gidsi",
+  DB_PASSWORD: "12345" || "gidsi12345",
   DB_NAME: "MPF_Gestion_De_Actas",
   DB_PORT: 3306,
 };
