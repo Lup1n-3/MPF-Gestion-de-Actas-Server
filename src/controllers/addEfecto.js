75bdc17c0b2340d79c13d46abdb7b35b5eee0e7d
Ramiro <ramifazio@gmail.com>
2023-01-25 10:31:20 -0300

    Update efectos endpoint

--- src/controllers/addEfecto.js
+++ src/controllers/addEfecto.js
@@ -9,55 +9,59 @@ addEfecto.post("/", async (req, res) => {
   const sds = req.body.sds;
 
   try {
+    //* Actualizo los estados de la bolsa
     const bolsa = await Bolsa.findByPk(bolsa_id);
-    if (req.body.efecto.estado === "completo") {
-      bolsa.estado = "abierta con efectos completos";
-    } else {
-      bolsa.estado = "abierta con efectos en proceso";
+    if (bolsa) {
+      if (req.body.efecto.estado === "completo") {
+        bolsa.estado = "abierta con efectos completos";
+      } else {
+        bolsa.estado = "abierta con efectos en proceso";
+      }
+      bolsa.save();
     }
-
-    //! Aca falta contemplar cuando dentro de una bolsa hay mujchos efectos con distintos estados, deberia ser en proceso. pero si estan todos completos seria estado = "abiertaa con efectos completos"
-
-    bolsa.save();
 
     const newEfecto = await Efecto.create({
       bolsa_id: bolsa_id,
       ...req.body.efecto,
     });
-
-    //* Creo los sims con FK al efecto
-    sims.forEach(async (s) => {
-      await Sim.create({
-        efecto_id: newEfecto.id,
-        empresaSim: s.empresaSim,
-        serialSim: s.serialSim,
-        tipoExtraccionSim: s.tipoExtraccionSim,
+    if (newEfecto) {
+      //* Creo los sims con FK al efecto
+      await sims.forEach(async (s) => {
+        await Sim.create({
+          efecto_id: newEfecto.id,
+          empresaSim: s.empresaSim,
+          serialSim: s.serialSim,
+          tipoExtraccionSim: s.tipoExtraccionSim,
+        });
       });
-    });
 
-    //* Creo los discos con FK al efecto
-    discos.forEach(async (d) => {
-      await Disco.create({
-        efecto_id: newEfecto.id,
-        tipoDeDisco: d.tipoDeDisco,
-        marca: d.marca,
-        modelo: d.modelo,
-        almacenamiento: d.almacenamiento,
-        serialNumber: d.serialNumber,
+      //* Creo los discos con FK al efecto
+      await discos.forEach(async (d) => {
+        await Disco.create({
+          efecto_id: newEfecto.id,
+          tipoDeDisco: d.tipoDeDisco,
+          marca: d.marca,
+          modelo: d.modelo,
+          almacenamiento: d.almacenamiento,
+          serialNumber: d.serialNumber,
+        });
       });
-    });
 
-    sds.forEach(async (s) => {
-      await Sd.create({
-        efecto_id: newEfecto.id,
-        marca: s.marca,
-        modelo: s.modelo,
-        almacenamiento: s.almacenamiento,
-        tipoExtraccionSd: s.tipoExtraccionSd,
+      //* Creo las Sds con FK al efecto
+      await sds.forEach(async (s) => {
+        await Sd.create({
+          efecto_id: newEfecto.id,
+          marca: s.marca,
+          modelo: s.modelo,
+          almacenamiento: s.almacenamiento,
+          tipoExtraccionSd: s.tipoExtraccionSd,
+        });
       });
-    });
+    }
 
-    return res.status(200).json(newEfecto);
+    const finalEfecto = await Efecto.findByPk(newEfecto.id, { include: { all: true } });
+
+    return res.status(200).json(finalEfecto);
   } catch (err) {
     console.log(err);
   }
