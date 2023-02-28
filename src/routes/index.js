6cebd7fbc44cd2c153774422dab982f95dcc2988
ramiro <ramifazio@gmail.com>
2023-02-28 11:41:12 -0300

    Order some things

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,41 +1,49 @@
 const router = require("express").Router();
 //* Importamos los Endpoints
+//* Post
 const addActa = require("../controllers/post/addActa");
-const getActas = require("../controllers/get/getActas");
 const addEfecto = require("../controllers/post/addEfecto");
-const getEfectos = require("../controllers/get/getEfectos");
 const addBolsa = require("../controllers/post/addBolsa");
 const addPeritos = require("../controllers/post/addPeritos");
 const addIntegrantes = require("../controllers/post/addIntegrantes");
-const updateBolsa = require("../controllers/put/updateBolsa");
-const updateActa = require("../controllers/put/updateActa");
-const removePerito = require("../controllers/delete/removePerito");
-const removeIntegrante = require("../controllers/delete/removeIntegrante");
 const createBugReport = require("../controllers/post/createBugReport");
+//* Get
+const getActas = require("../controllers/get/getActas");
+const getEfectos = require("../controllers/get/getEfectos");
 const getBugsReports = require("../controllers/get/getBugsReports");
 const getUpdatedBolsas = require("../controllers/get/getUpdatedBolsas");
-const removeBolsa = require("../controllers/delete/removeBolsa");
+//* Put
+const updateBolsa = require("../controllers/put/updateBolsa");
+const updateActa = require("../controllers/put/updateActa");
 const closeProcessActa = require("../controllers/put/closeProcessActa");
+//* Delete
+const removeIntegrante = require("../controllers/delete/removeIntegrante");
+const removePerito = require("../controllers/delete/removePerito");
+const removeBolsa = require("../controllers/delete/removeBolsa");
 const removeEfecto = require("../controllers/delete/removeEfecto");
 const removeActa = require("../controllers/delete/removeActa");
 
 //* Generamos las rutas
+//* Post
 router.use("/addActa", addActa);
+router.use("/addEfecto", addEfecto);
 router.use("/addBolsa", addBolsa);
 router.use("/addPeritos", addPeritos);
 router.use("/addIntegrantes", addIntegrantes);
-router.use("/addEfecto", addEfecto);
+router.use("/createBugReport", createBugReport);
+//* Get
 router.use("/getActas", getActas);
 router.use("/getEfectos", getEfectos);
-router.use("/updateBolsa", updateBolsa);
-router.use("/updateActa", updateActa);
-router.use("/removePerito", removePerito);
-router.use("/removeIntegrante", removeIntegrante);
-router.use("/createBugReport", createBugReport);
 router.use("/getBugsReports", getBugsReports);
 router.use("/getUpdatedBolsas", getUpdatedBolsas);
-router.use("/removeBolsa", removeBolsa);
+//* Put
+router.use("/updateBolsa", updateBolsa);
+router.use("/updateActa", updateActa);
 router.use("/closeProcessActa", closeProcessActa);
+//* Delete
+router.use("/removeIntegrante", removeIntegrante);
+router.use("/removePerito", removePerito);
+router.use("/removeBolsa", removeBolsa);
 router.use("/removeEfecto", removeEfecto);
 router.use("/removeActa", removeActa);
 
