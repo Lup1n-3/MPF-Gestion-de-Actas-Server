0f267b077eb8ffd9186cbb3ffdd65d51a20c7982
ramiro <ramifazio@gmail.com>
2023-03-09 15:05:46 -0300

    Min fix

--- src/controllers/post/addPeritos.js
+++ src/controllers/post/addPeritos.js
@@ -3,9 +3,9 @@ const { Perito } = require("../../db");
 
 addPeritos.post("/", async (req, res) => {
   try {
-    const newPerito = await Perito.bulkCreate(req.body);
+    const newPeritos = await Perito.bulkCreate(req.body);
 
-    return res.status(200).json(newPerito);
+    return res.status(200).json(newPeritos);
   } catch (err) {
     console.log(err);
     return res.status(500).send(err);
