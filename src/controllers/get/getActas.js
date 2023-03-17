743b21b272ea7c96239c323f084fe43665efc57a
ramiro <ramifazio@gmail.com>
2023-03-17 10:54:20 -0300

    FIx bug of update acta

--- src/controllers/get/getActas.js
+++ src/controllers/get/getActas.js
@@ -5,10 +5,10 @@ getActas.get("/", async (req, res) => {
   try {
     const allActas = await Acta.findAll({ include: { all: true, nested: true } }); //* Me traigo todas las actas con todas sus relaciones
 
-    return res.status(200).send(allActas);
+    return res.status(200).json(allActas);
   } catch (err) {
     console.log(err);
-    return res.status(500).send(err);
+    return res.status(500).json(err);
   }
 });
 
@@ -18,10 +18,12 @@ getActas.get("/:id", async (req, res) => {
   try {
     const acta = await Acta.findByPk(id, { include: { all: true, nested: true } }); //* Me traigo el acta con todas sus relaciones
 
-    return res.status(200).send(acta);
+    console.log("-----> ACTA", acta);
+
+    return res.status(200).json(acta);
   } catch (err) {
     console.log(err);
-    return res.status(500).send(err);
+    return res.status(500).json(err);
   }
 });
 
